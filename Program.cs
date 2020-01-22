e7f7c3eeba94b827106fb2319fc2d2995830e712
Freddie Fellowes <ffellowes@hotmail.co.uk>
2020-01-22 13:42:05 +0000

    figure out why src isn't uploaded, only .git*

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_git_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("git1");
+
+            // section 1
+            ;
+
+            // section 2
+            ;
+
+            // section 3
+            ;
+
+            Console.ReadLine();
+        }
+    }
+}
